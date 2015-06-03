cc91b7469012e57fcd2d00ed3dacd93dff508ffd
Alexandr Fedorov <alexndr.fedorov@gmail.com>
2015-06-03 16:05:17 +0300

    Start warranty spec

---
+++ spec/warranty_spec.rb
@@ -0,0 +1,21 @@
+require 'warranty'
+
+RSpec.describe Warranty do
+
+  describe '#expiration_date' do
+
+    context 'for IMEI with active warranty' do
+      it 'returns the expiration date' do
+        imei = '013977000323877'
+        stub_request(:get, "selfsolve.apple.com/wcResults.do?sn=#{imei}&num=0").
+          to_return(read_fixture(imei))
+
+        warranty = Warranty.new(imei)
+        expiration_date = Date.new('2015, 8, 10')
+        expect(warranty.expiration_date).to eq(expiration_date)
+      end
+    end
+
+  end
+
+end
