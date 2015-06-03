20e52be2e473e438b8e842c7f0a79d4f4c374cc3
Alexandr Fedorov <alexndr.fedorov@gmail.com>
2015-06-03 16:24:25 +0300

    Add implementation for successful date extraction

--- lib/warranty.rb
+++ lib/warranty.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+
+class ParsingError < StandardError; end
+class InvalidImeiError < StandardError; end
+
 class Warranty
 
   def initialize(imei)
@@ -15,9 +20,22 @@ class Warranty
   end
 
   def scrape_page
+    uri = URI('https://selfsolve.apple.com/wcResults.do')
+    uri.query = URI.encode_www_form({sn: @imei, num: 0}.to_a)
+    uri.read
   end
 
   def extract_warranty_date
+    case details
+    when /warrantyPage.warrantycheck.displayHWSupportInfo\s*\(\s*true(.*)\)\;/
+      if matches = $1.match(/Expiration Date\:\s*(.*\D\d{4})\D/)
+        Date.parse(matches[0])
+      else
+        raise(ParsingError, 'Can\'t parse the page')
+      end
+    else
+      raise(ParsingError, 'Can\'t parse the page')
+    end
   end
 
 end
--- spec/warranty_spec.rb
+++ spec/warranty_spec.rb
@@ -5,13 +5,16 @@ RSpec.describe Warranty do
   describe '#expiration_date' do
 
     context 'for IMEI with active warranty' do
-      it 'returns the expiration date' do
-        imei = '013977000323877'
-        stub_request(:get, "selfsolve.apple.com/wcResults.do?sn=#{imei}&num=0").
-          to_return(read_fixture(imei))
+      before do
+        @imei = '013977000323877'
+        stub_request(:get, "https://selfsolve.apple.com/wcResults.do"\
+          "?sn=#{@imei}&num=0").to_return(read_fixture(@imei))
+      end
 
-        warranty = Warranty.new(imei)
-        expiration_date = Date.new(2015, 8, 10)
+      it 'returns the expiration date' do
+        warranty = Warranty.new(@imei)
+        expiration_date = Date.new(2016, 8, 10)
+
         expect(warranty.expiration_date).to eq(expiration_date)
       end
     end
